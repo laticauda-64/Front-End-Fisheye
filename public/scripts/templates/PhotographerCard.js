4be24906e835c988eaf6a3e5bf25e8419fceffed
YoannBDL <yoannb64@gmail.com>
2021-11-13 13:59:20 +0100

    Small CSS fix on BEM

--- public/scripts/templates/PhotographerCard.js
+++ public/scripts/templates/PhotographerCard.js
@@ -12,17 +12,17 @@ class PhotographerCard {
         const picture = `public/assets/photographers/${portrait}`;
 
         const article = document.createElement('article');
-        article.classList.add('photographersSection_profile');
+        article.classList.add('photographersSection__profile');
         article.dataset.id = id;
 
         const content = `
-            <a class="photographersSection_profile_link" href="photographer.html?id=${id}">
-                <img src="${picture}" class="photographersSection_profile_link_picture" alt=""></img>
+            <a class="photographersSection__profile__link" href="photographer.html?id=${id}">
+                <img src="${picture}" class="photographersSection__profile__link__picture" alt=""></img>
                 <h2 class="photographersSection_profile_link_name">${name}</h2>
             </a>
-            <p class="photographersSection_profile_location">${city}, ${country}</p>
-            <p class="photographersSection_profile_description">${tagline}</p>
-            <p class="photographersSection_profile_price">${price}€/jour</p>
+            <p class="photographersSection__profile__location">${city}, ${country}</p>
+            <p class="photographersSection__profile__description">${tagline}</p>
+            <p class="photographersSection__profile__price">${price}€/jour</p>
         `;
 
         article.innerHTML = content;
