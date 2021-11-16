6f88d1cbfff770963ef4bd75654fef7f6222af6c
YoannBDL <yoannb64@gmail.com>
2021-11-16 17:13:09 +0100

    Fixing Api Fetch relative path for Github pages

--- public/scripts/api/Api.js
+++ public/scripts/api/Api.js
@@ -1,6 +1,6 @@
 class Api {
     async fetchData() {
-        return fetch('http://127.0.0.1:5500/data/photographers.json')
+        return fetch('data/photographers.json')
             .then((res) => res.json())
             .then((res) => res)
             .catch((err) => console.log('an error occurs', err));
