7163012ee05c5025bdb6404e387e02e7fc2b3c1f
YoannBDL <yoannb64@gmail.com>
2021-11-16 15:44:11 +0100

    Contact Form fully done

--- public/scripts/utils/switchContactForm.js
+++ public/scripts/utils/switchContactForm.js
@@ -6,16 +6,48 @@
 class SwitchContactModal {
     constructor() {
         this._modalOpen = false;
-        this.$modalForm = document.getElementById('contact_modal');
+        this.$modalForm = document.querySelector('.contactModal');
         this.$contactButton = document.querySelector('.photographer__profileCard__contactButton');
+        this.$closeButton = document.querySelector('.contactModal__content__header__closeIcon');
+        this.$submitButton = document.querySelector('.contactModal__content__form__submit');
     }
 
-    triggerModal = () => {
-        this._modalOpen = !this._modalOpen;
-        this.$modalForm.style.display = this._modalOpen ? 'block' : 'none';
+    triggerModal = (event) => {
+        switch (event.type) {
+            case 'click':
+                this._modalOpen = !this._modalOpen;
+                if (this._modalOpen) {
+                    this.$modalForm.style.display = 'flex';
+                    document.documentElement.addEventListener('keydown', this.triggerModal);
+                } else {
+                    this.$modalForm.style.display = 'none';
+                    document.documentElement.removeEventListener('keydown', this.triggerModal);
+                }
+                break;
+
+            case 'keydown':
+                if (event.keyCode === 27) {
+                    this.triggerModal({ type: 'click' });
+                }
+                break;
+
+            default:
+                break;
+        }
     };
+
+    submitForm(event) {
+        event.preventDefault();
+        const formData = new FormData(document.querySelector('.contactModal__content__form'));
+        // Affichage des champs du formulaire dans la console
+        for (var pair of formData.entries()) {
+            console.log(pair[0] + ': ' + pair[1]);
+        }
+    }
 
     addListeners() {
         this.$contactButton.addEventListener('click', this.triggerModal);
+        this.$closeButton.addEventListener('click', this.triggerModal);
+        this.$submitButton.addEventListener('click', this.submitForm);
     }
 }
