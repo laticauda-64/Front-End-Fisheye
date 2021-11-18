d05facf4152ec461ed9730fe035341041c6d959d
YoannBDL <yoannb64@gmail.com>
2021-11-18 13:35:54 +0100

    Builded a nice looking select in filerform which is accessible too

--- public/scripts/templates/FilterForm.js
+++ public/scripts/templates/FilterForm.js
@@ -7,7 +7,169 @@ class FilterForm {
         this._work = work;
         this.$wrapper = document.createElement('section');
         this.$main = document.getElementById('main');
+
+        // Customized select dropdown
+        this.$selectNative;
+        this.$selectCustom;
+        this.$selectCustomTrigger;
+        this.$selectCustomOpts;
+        this.customOptsList;
+        this.optionsCount;
+        this.defaultLabel;
+
+        this.optionChecked = '';
+        this.optionHoveredIndex = -1;
     }
+
+    customizedDropDownMenu = () => {
+        this.$selectNative = document.querySelector('.filter__selectNative');
+        this.$selectCustom = document.querySelector('.filter__selectCustom');
+        this.$selectCustomTrigger = document.querySelector('.filter__selectCustom__trigger');
+        this.$selectCustomOpts = document.querySelector('.filter__selectCustom__options');
+        this.customOptsList = Array.from(this.$selectCustomOpts.children);
+        this.optionsCount = this.customOptsList.length;
+        this.defaultLabel = this.$selectCustomTrigger.getAttribute('data-value');
+
+        const openSelectCustom = () => {
+            this.$selectCustom.classList.add('isActive');
+            // Remove aria-hidden in case this was opened by a user
+            // who uses AT (e.g. Screen Reader) and a mouse at the same time.
+            this.$selectCustom.setAttribute('aria-hidden', false);
+
+            if (this.optionChecked) {
+                const optionCheckedIndex = this.customOptsList.findIndex((el) => el.getAttribute('data-value') === this.optionChecked);
+                updateCustomSelectHovered(optionCheckedIndex);
+            }
+
+            // Add related event listeners
+            document.addEventListener('click', watchClickOutside);
+            document.addEventListener('keydown', supportKeyboardNavigation);
+        };
+
+        const closeSelectCustom = () => {
+            this.$selectCustom.classList.remove('isActive');
+
+            this.$selectCustom.setAttribute('aria-hidden', true);
+
+            updateCustomSelectHovered(-1);
+
+            // Remove related event listeners
+            document.removeEventListener('click', watchClickOutside);
+            document.removeEventListener('keydown', supportKeyboardNavigation);
+        };
+
+        const updateCustomSelectHovered = (newIndex) => {
+            const prevOption = this.$selectCustomOpts.children[this.optionHoveredIndex];
+            const option = this.$selectCustomOpts.children[newIndex];
+
+            if (prevOption) {
+                prevOption.classList.remove('isHover');
+            }
+            if (option) {
+                option.classList.add('isHover');
+            }
+
+            this.optionHoveredIndex = newIndex;
+        };
+
+        const updateCustomSelectChecked = (value, text) => {
+            const prevValue = this.optionChecked;
+
+            const elPrevOption = this.$selectCustomOpts.querySelector(`[data-value="${prevValue}"`);
+            const elOption = this.$selectCustomOpts.querySelector(`[data-value="${value}"`);
+
+            if (elPrevOption) {
+                elPrevOption.classList.remove('isActive');
+            }
+
+            if (elOption) {
+                elOption.classList.add('isActive');
+            }
+
+            this.$selectCustomTrigger.textContent = text;
+            this.optionChecked = value;
+        };
+
+        const watchClickOutside = (e) => {
+            const didClickedOutside = !this.$selectCustom.contains(event.target);
+            if (didClickedOutside) {
+                closeSelectCustom();
+            }
+        };
+
+        const supportKeyboardNavigation = (e) => {
+            // press down -> go next
+            if (event.keyCode === 40 && this.optionHoveredIndex < this.optionsCount - 1) {
+                let index = this.optionHoveredIndex;
+                e.preventDefault(); // prevent page scrolling
+                updateCustomSelectHovered(this.optionHoveredIndex + 1);
+            }
+
+            // press up -> go previous
+            if (event.keyCode === 38 && this.optionHoveredIndex > 0) {
+                e.preventDefault(); // prevent page scrolling
+                updateCustomSelectHovered(this.optionHoveredIndex - 1);
+            }
+
+            // press Enter or space -> select the option
+            if (event.keyCode === 13 || event.keyCode === 32) {
+                e.preventDefault();
+
+                const option = this.$selectCustomOpts.children[this.optionHoveredIndex];
+                const value = option && option.getAttribute('data-value');
+
+                if (value) {
+                    this.$selectNative.value = value;
+                    updateCustomSelectChecked(value, option.textContent);
+                }
+                closeSelectCustom();
+            }
+
+            // press ESC -> close selectCustom
+            if (event.keyCode === 27) {
+                closeSelectCustom();
+            }
+        };
+
+        // Events listenners
+        // Update selectCustom value when selectNative is changed.
+        this.$selectNative.addEventListener('change', (e) => {
+            const value = e.target.value;
+            const elRespectiveCustomOption = this.$selectCustomOpts.querySelectorAll(`[data-value="${value}"]`)[0];
+
+            updateCustomSelectChecked(value, elRespectiveCustomOption.textContent);
+        });
+
+        // Toggle custom select visibility when clicking the box
+        this.$selectCustomTrigger.addEventListener('click', (e) => {
+            const isClosed = !this.$selectCustom.classList.contains('isActive');
+
+            if (isClosed) {
+                openSelectCustom();
+            } else {
+                closeSelectCustom();
+            }
+        });
+
+        // Imperative code
+        // Update selectCustom value when an option is clicked or hovered
+        this.customOptsList.forEach((elOption, index) => {
+            elOption.addEventListener('click', (e) => {
+                const value = e.target.getAttribute('data-value');
+
+                // Sync native select to have the same value
+                this.$selectNative.value = value;
+                updateCustomSelectChecked(value, e.target.textContent);
+                closeSelectCustom();
+            });
+
+            elOption.addEventListener('mouseenter', (e) => {
+                updateCustomSelectHovered(index);
+            });
+
+            // TODO: Toggle these event listeners based on selectCustom visibility
+        });
+    };
 
     onChangeFilter() {
         // Ici event listenners pour gérer la sélection d'un critère de filtre
@@ -22,17 +184,31 @@ class FilterForm {
         this.$wrapper.classList.add('filter');
 
         const filterForm = `
-                    <label for="Order-By" class="filter__label">Trier par</label>
-                    <select name="Order-By" id="Order-By" class="filter__input">
-                        <option value="popular">Popularité</option>
-                        <option value="date">Date</option>
-                        <option value="title">Titre</option>
-                    </select>
+                    <span class="filter__label" id="sortBy">Trier par</span>
+                    <div class="filter__wrapper">
+                        <!-- The native select for assistive tech -->
+                        <select class="filter__selectNative" aria-labelledby="sortBy">
+                            <option value="popular">Popularité</option>
+                            <option value="date">Date</option>
+                            <option value="title">Titre</option>
+                        </select>
+                        <!-- The beautiful custom select -->
+                        <div class="filter__selectCustom" aria-hidden="true">
+                            <div class="filter__selectCustom__trigger">Popularité</div>
+                            <div class="filter__selectCustom__options"> 
+                                <div class="filter__selectCustom__option" data-value="popular">Popularité</div>
+                                <div class="filter__selectCustom__option" data-value="date">Date</div>
+                                <div class="filter__selectCustom__option" data-value="title">Titre</div>
+                            </div>
+                        </div>
+                    </div>
+
         `;
 
         this.$wrapper.innerHTML = filterForm;
         // this.onChangeFilter();
 
         this.$main.appendChild(this.$wrapper);
+        this.customizedDropDownMenu();
     }
 }
